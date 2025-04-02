5599cf493006faf4dc8f14102be9ec0a0333ec2f
abdelhadi <abdelhadi.alaouibalghiti@edu.ece.fr>
2025-04-02 11:31:01 +0200

    diagramme de classe

--- Controleur/CandidatureController.java
+++ Controleur/CandidatureController.java
@@ -1,4 +1,33 @@
 package Controleur;
+import DAO.OffreEmploiDAO;
+import DAO.CandidatureDAO;
+import DAO.DemandeurEmploiDAO;
+import Modele.Candidature;
+
+import java.util.List;
+import java.util.Map;
 
 public class CandidatureController {
+    private CandidatureDAO candidatureDAO;
+    private DemandeurEmploiDAO demandeurDAO;
+    private OffreEmploiDAO offreDAO;
+
+    public CandidatureController(CandidatureDAO candidatureDAO, DemandeurEmploiDAO demandeurDAO, OffreEmploiDAO offreDAO) {
+        this.candidatureDAO = candidatureDAO;
+        this.demandeurDAO = demandeurDAO;
+        this.offreDAO = offreDAO;
+    }
+
+    public boolean créerCandidature(Candidature candidature) {
+        return false;
+    }
+
+    public boolean modifierStatut(int id, String statut) {
+        return false;
+    }
+
+    public List<Candidature> consulterCandidatures(Map<String, String> filtres) {
+        return null;
+    }
 }
+
--- Controleur/OffreEmploiController.java
+++ Controleur/OffreEmploiController.java
@@ -1,4 +1,29 @@
 package Controleur;
+import DAO.CategorieDAO;
+import DAO.OffreEmploiDAO;
+import Modele.OffreEmploi;
 
 public class OffreEmploiController {
-}
+
+        private OffreEmploiDAO offreEmploiDAO;
+        private CategorieDAO categorieDAO;
+
+        public OffreEmploiController(OffreEmploiDAO offreDAO, CategorieDAO categorieDAO) {
+            this.offreEmploiDAO = offreDAO;
+            this.categorieDAO = categorieDAO;
+        }
+
+        public boolean créerOffre(OffreEmploi offre) {
+            return false;
+        }
+
+        public boolean modifierOffre(OffreEmploi offre) {
+            return false;
+        }
+
+        public boolean supprimerOffre(int id) {
+            return false;
+        }
+    }
+
+

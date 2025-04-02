91c397cf807678e4463d118ef5148106a57f7643
AntoineLal <116791323+AntoineLal@users.noreply.github.com>
2025-04-02 11:07:37 +0200

    test

--- Modele/Employeur.java
+++ Modele/Employeur.java
@@ -1,4 +1,54 @@
 package Modele;
 
 public class Employeur {
+    private String nomEntreprise;
+    private String secteurActivite;
+    private String adresse;
+    private String telephone;
+    private String email;
+    private String description;
+
+    // tous les getter
+
+    public String getNomEntreprise() {
+        return nomEntreprise;
+    }
+    public String getSecteurActivite() {
+        return secteurActivite;
+    }
+    public String getAdresse() {
+        return adresse;
+    }
+    public String getTelephone() {
+        return telephone;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+    // tous les setter
+
+    public void setNomEntreprise(String nomEntreprise) {
+        this.nomEntreprise = nomEntreprise;
+    }
+    public void setSecteurActivite(String secteurActivite) {
+        this.secteurActivite = secteurActivite;
+    }
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
 }
--- Modele/OffreEmploi.java
+++ Modele/OffreEmploi.java
@@ -1,4 +1,14 @@
 package Modele;
+import java.util.*;
 
 public class OffreEmploi {
+    public int id;
+    public String title;
+    public String description;
+    public ArrayList<String> competences;
+    public double salaire;
+    public Date datePublication;
+    public String statut;
+    public String localisation;
+    public String typeContrat;
 }

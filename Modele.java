0c6bf709962b9817aa9ac7e0e570797996c57fa2
Ranya STITOU <ranyast@yahoo.fr>
2025-04-02 10:04:46 +0200

    Ajout des packages

--- Modele.java
+++ Modele.java
@@ -1,4 +1,3 @@
 public class Modele {
     private int argent;
-    private int ranyaMechante;
 }
---
+++ Vue/main.java
@@ -0,0 +1,7 @@
+package Vue;
+
+public class main {
+    public static void main(String[] args) {
+
+    }
+}

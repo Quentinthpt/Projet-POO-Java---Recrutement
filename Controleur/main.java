ebfcae9a71c2ccf9db20237e459b7a754386b3e1
Ranya STITOU <ranyast@yahoo.fr>
2025-04-02 11:18:23 +0200

    Déplacer le fichier main dans le controlleur

---
+++ Controleur/main.java
@@ -0,0 +1,13 @@
+package Controleur;
+
+import java.sql.*;
+import DAO.*;
+import Modele.*;
+import Vue.*;
+import java.util.ArrayList;
+
+public class main {
+    public static void main(String[] args) {
+        new MainPage();
+    }
+}

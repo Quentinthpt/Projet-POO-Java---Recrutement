5a006cd87003ae4df124c217850763b90905db9d
abdelhadi <abdelhadi.alaouibalghiti@edu.ece.fr>
2025-04-02 11:15:20 +0200

    diagramme de classe

--- Controleur/AuthentificationController.java
+++ Controleur/AuthentificationController.java
@@ -1,4 +1,32 @@
 package Controleur;
 
+import DAO.UtilisateurDAO;
+import Modele.Utilisateur;
+
 public class AuthentificationController {
+    private UtilisateurDAO utilisateurDAO;
+
+    public AuthentificationController(UtilisateurDAO utilisateurDAO) {
+        this.utilisateurDAO = utilisateurDAO;
+    }
+
+    public UtilisateurDAO getUtilisateurDAO() {
+        return utilisateurDAO;
+    }
+    public Utilisateur connecter(String login, String password) {
+        // Implémentation à faire
+        return null;
+    }
+    public boolean inscrire(Utilisateur utilisateur){
+        return false;
+    }
+    public boolean deconnecter() {
+        // Implémentation à faire
+        return false;
+    }
+
+    public boolean récupérerMotDePasse(String email) {
+        // Implémentation à faire
+        return false;
+    }
 }

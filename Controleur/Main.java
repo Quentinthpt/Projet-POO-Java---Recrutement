96d4571e16ffb76c7425cbe3ea3582c0c9343756
Ranya STITOU <ranyast@yahoo.fr>
2025-04-09 12:59:47 +0200

    Création de la page Inscription + Connexion = basique

--- Controleur/Main.java
+++ Controleur/Main.java
@@ -6,8 +6,17 @@ import Modele.*;
 import Vue.*;
 import java.util.ArrayList;
 
+import static javafx.application.Application.launch;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 public class Main {
     public static void main(String[] args) {
-        new MainPage();
+        MainPage.main(args);
+        //launch(args);
+        //new MainPage();
     }
 }

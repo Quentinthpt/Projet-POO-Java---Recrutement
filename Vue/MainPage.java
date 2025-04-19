3747df1488d8634aa9baf7901b5df52e9f411f71
Quentinthpt <quentin.thepaut@edu.ece.fr>
2025-04-19 14:54:19 +0200

    Miase a jour de la page d'accueil avec de nouvelels photos

--- Vue/MainPage.java
+++ Vue/MainPage.java
@@ -43,6 +43,12 @@ public class MainPage extends JFrame {
         setVisible(true);
     }
 
+    private ImageIcon resizeImage(ImageIcon icon, int width, int height) {
+        Image img = icon.getImage();
+        Image resizedImage = img.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        return new ImageIcon(resizedImage);
+    }
+
     private JPanel createContentPanel() {
         Color bleuFonce = new Color(9, 18, 66);
         Color blanc = Color.WHITE;
@@ -55,37 +61,43 @@ public class MainPage extends JFrame {
         corps_de_la_page.setBackground(bleuFonce);
         corps_de_la_page.setBorder(new EmptyBorder(10, 10, 10, 10));
 
-        // Case 1.1 - Image d’accroche
-        ImageIcon corps_1_1 = new ImageIcon("images/telechargement2.png");
+        // Dimensions communes pour les images
+        int imageWidth = 400;
+        int imageHeight = 300;
+
+        // Case 1.1 - Image d'accroche
+        ImageIcon corps_1_1 = new ImageIcon("images/groupe.jpg");
+        corps_1_1 = resizeImage(corps_1_1, imageWidth, imageHeight);
         JLabel img_1_1 = new JLabel(corps_1_1, JLabel.CENTER);
         img_1_1.setBorder(BorderFactory.createEmptyBorder(20, 0, 20, 0));
         corps_de_la_page.add(img_1_1);
 
-// Case 1.2 - Message accrocheur
+        // Case 1.2 - Message accrocheur
         JLabel corps_1_2 = new JLabel("<html><div style='width: 400px;'>"
-                + "<h2 style='color: #ffffff;'>Trouver un job n’a jamais été aussi simple !</h2>"
-                + "<p style='font-size: 14px;'>Chez nous, vous êtes bien plus qu’un simple CV. "
-                + "Notre équipe de conseillers passionnés vous accompagne à chaque étape, du choix de l’offre à l’entretien final. "
+                + "<h2 style='color: #ffffff;'>Trouver un job n'a jamais été aussi simple !</h2>"
+                + "<p style='font-size: 14px;'>Chez nous, vous êtes bien plus qu'un simple CV. "
+                + "Notre équipe de conseillers passionnés vous accompagne à chaque étape, du choix de l'offre à l'entretien final. "
                 + "Fini les arnaques et les promesses floues : chaque annonce est vérifiée, chaque recruteur est authentifié.</p>"
                 + "<p style='margin-top: 10px;'>Notre objectif ? Que vous trouviez rapidement un emploi qui vous correspond, "
-                + "et que vous n’ayez plus besoin de nous 😉</p>"
+                + "et que vous n'ayez plus besoin de nous 😉</p>"
                 + "</div></html>", JLabel.LEFT);
         corps_1_2.setFont(new Font("SansSerif", Font.PLAIN, 15));
         corps_1_2.setForeground(blanc);
         corps_1_2.setBorder(BorderFactory.createEmptyBorder(0, 10, 30, 10));
         corps_de_la_page.add(corps_1_2);
 
-// Case 2.1 - Deuxième image
-        ImageIcon corps_2_1 = new ImageIcon("images/telechargement2.png");
+        // Case 2.1 - Deuxième image
+        ImageIcon corps_2_1 = new ImageIcon("images/marche.jpeg");
+        corps_2_1 = resizeImage(corps_2_1, imageWidth, imageHeight);
         JLabel img_2_1 = new JLabel(corps_2_1, JLabel.CENTER);
         img_2_1.setBorder(BorderFactory.createEmptyBorder(20, 0, 20, 0));
         corps_de_la_page.add(img_2_1);
 
-// Case 2.2 - Infos + conseils
+        // Case 2.2 - Infos + conseils
         JLabel corps_2_2 = new JLabel("<html><div style='width: 400px;'>"
                 + "<h2 style='color: #ffffff;'>Boostez votre avenir professionnel</h2>"
                 + "<p style='font-size: 14px;'>Accédez à des contenus exclusifs : conseils pour réussir vos entretiens, "
-                + "astuces pour valoriser votre profil, et décryptage des tendances du marché de l’emploi.</p>"
+                + "astuces pour valoriser votre profil, et décryptage des tendances du marché de l'emploi.</p>"
                 + "<p style='margin-top: 10px;'>Chaque jour, de nouvelles opportunités vous attendent. "
                 + "Notre plateforme intelligente vous recommande les meilleures offres, adaptées à vos compétences et à vos envies.</p>"
                 + "</div></html>", JLabel.LEFT);
@@ -97,11 +109,7 @@ public class MainPage extends JFrame {
         contentPanel.add(corps_de_la_page);
         contentPanel.add(Box.createVerticalStrut(20));
 
-        JLabel offresLabel = new JLabel("486 offres d'emploi : services aux entreprises");
-        offresLabel.setForeground(blanc);
-        offresLabel.setFont(new Font("SansSerif", Font.PLAIN, 18));
-        offresLabel.setBorder(new EmptyBorder(20, 20, 10, 0));
-        contentPanel.add(offresLabel);
+
 
         return contentPanel;
     }
